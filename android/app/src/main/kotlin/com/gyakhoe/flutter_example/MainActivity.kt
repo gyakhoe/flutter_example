3da4ca3b5502d4b5b44bf4b6e99fd6fe13f8d60a
Gyague Sonam <gyakhoe@MacBook-Air.local>
2020-06-02 09:08:51 +0545

    Starter project for flutter example. Removed all comments and created welcome page

---
+++ android/app/src/main/kotlin/com/gyakhoe/flutter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gyakhoe.flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/example/flutter_example.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FlutterExample extends StatelessWidget {
+  const FlutterExample({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Welcome To Flutter Example App.'),
+    );
+  }
+}
